4c134afdb00d7f3a3fe72ac586d47cbeb8835879
Kwon <Kimaku486@gmail.com>
2022-02-24 01:11:39 +0900

    if

--- Input-output/If.py
+++ Input-output/If.py
@@ -107,3 +107,22 @@
 # import random
 # a = random.randint(1, 2147483647)
 # print(a)
+
+# A,B,C = map(int, input().split()) #같은 갯수 주사위
+# a = [A,B,C]
+# if (1<= A <= 6) and (1<= A <= 6) and (1<= A <= 6):
+#   if (A+B+C)/3 == A:
+#     if B == C:
+#       print(10000+A*1000)
+#     else:
+#       print(max(a)*100)
+#   else:
+#     if A==B or A==C:
+#       print(1000+A*100)
+#     else:
+#       if B == C:
+#         print(1000+B*100)
+#       else:
+#         print(max(a)*100)
+# else:
+#   print("값오류")
