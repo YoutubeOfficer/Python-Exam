16f7196b7fec1b64d985a3166c7b0f62a3371b20
Kwon <Kimaku486@gmail.com>
2022-03-05 02:26:40 +0900

    while문

---
+++ Input-output/while.py
@@ -0,0 +1,16 @@
+# a,b = list(map(int, input().split()))
+
+# while 0 < a and b < 10 :
+#   print(a+b)
+#   a,b = list(map(int, input().split()))
+#   if a == 0 and b == 0 :
+#     break
+
+# a,b = list(map(int, input().split()))
+
+# while 0 < a and b < 10 :
+#   try:
+#     print(a+b)
+#     a,b = map(int, input().split())
+#   except:
+#     break
